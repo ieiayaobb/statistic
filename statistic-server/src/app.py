f89b2ca0aa140197b3af490b215843c89e04790f
ieiayaobb <ieiayaobb@gmail.com>
2015-07-14 16:46:26 +0800

    first init

---
+++ statistic-server/src/app.py
@@ -0,0 +1,11 @@
+__author__ = 'shenyineng'
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
